24cf9737b963fd965467f4e62a7c2ca9f90980be
jakariaparvezz <you@example.com>
2025-12-16 12:40:17 +0600

    ready to display phones

--- js/api.js
+++ js/api.js
@@ -1,5 +1,31 @@
 function loadData(){
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => showingPhone(data.data))
+}
+
+const showingPhone = phones =>{
+    const container = document.getElementById('phone-container')
+            phones.forEach(phone => {
+                console.log(phone)
+                const div = document.createElement('div')
+                div.classList = `card bg-base-200 w-96 shadow-sm`
+
+                div.innerHTML = `
+                    <figure class="px-10 pt-10">
+                        <img
+                        src=${phone.image}
+                        alt="Shoes"
+                        class="rounded-xl" />
+                     </figure>
+                <div class="card-body items-center text-center">
+                            <h2 class="card-title">${phone.phone_name}</h2>
+                            <p>${phone.slug}</p>
+                        <div class="card-actions">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                </div> `
+
+                container.appendChild(div)
+        });
 }
