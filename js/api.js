0b1ebc0a0ed3a1767111ef12efd3cdae04ab48d3
jakariaparvezz <you@example.com>
2025-12-16 17:44:50 +0600

    show details section done

--- js/api.js
+++ js/api.js
@@ -1,11 +1,10 @@
-
 function call(){
     const searchText = document.getElementById('searchText').value
-    console.log(searchText)
+    
     loadData(searchText)
 }
 
-function loadData(text){
+function loadData(text='iphone'){
     fetch(`https://openapi.programming-hero.com/api/phones?search=${text}`)
     .then(res => res.json())
     .then(data => showingPhone(data.data))
@@ -40,9 +39,10 @@ const showingPhone = phones =>{
 
                     <div class="card-body items-center text-center">
                             <h2 class="card-title">${phone.phone_name}</h2>
-                            <p>${phone.slug}</p>
                         <div class="card-actions">
-                            <button class="btn btn-primary">Buy Now</button>
+
+                            <button  onclick="callDetails('${phone.slug}');my_modal.showModal()" class="btn btn-primary">Show Details</button>
+
                         </div>
                     </div> 
                 `
@@ -50,5 +50,48 @@ const showingPhone = phones =>{
                 container.appendChild(div)
         });
 
-
 }
+
+const callDetails =async id =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    let data = await res.json()
+    data = data.data
+    showDetails(data)
+    
+}
+
+const showDetails = details =>{
+    console.log(details)
+    const detailsContainer = document.getElementById('details-container')
+
+    detailsContainer.innerHTML = `<figure>
+                                  <img
+                                    src="${details.image}"
+                                    alt="Shoes" />
+                                </figure>
+                                <div class="card-body">
+                                  <h2 class="card-title text-2xl font-bold">${details.name}</h2>
+                                  <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                                  <p><span class="font-medium text-base">Brand : </span class="font-medium">${details.brand}</p>
+                                  
+                                  <p><span class="font-medium text-base">ChipSet : </span class="font-medium">${details.mainFeatures.chipSet}</p>
+
+                                  <p><span class="font-medium text-base">Display Size : </span class="font-medium">${details.mainFeatures.displaySize}</p>
+
+                                  <p><span class="font-medium text-base">Memory: </span class="font-medium">${details.mainFeatures.memory}</p>
+
+                                  <p><span class="font-medium text-base">Storage : </span class="font-medium">${details.mainFeatures.storage}</p>
+
+                                  <p><span class="font-medium text-base">Release Date : </span class="font-medium">${details.releaseDate}</p>
+                                  
+
+                                  <div class="card-actions justify-end">
+                                    <button class="btn btn-primary">Buy Now</button>
+                                  </div>
+                                </div>`
+}
+
+
+
+loadData()
